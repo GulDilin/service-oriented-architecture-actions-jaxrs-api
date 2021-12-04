3558e07170fdfd13eb54dc79e7a32605cc226e97
GulDilin <zhenyagurin@gmail.com>
2021-12-04 14:17:12 +0300

    Add exceptions and exception mappers

--- src/main/java/guldilin/exceptions/StorageServiceRequestException.java
+++ src/main/java/guldilin/exceptions/StorageServiceRequestException.java
@@ -4,4 +4,8 @@ public class StorageServiceRequestException extends Throwable{
     public StorageServiceRequestException(String message) {
         super(message);
     }
+
+    public StorageServiceRequestException() {
+        super(ErrorMessage.STORAGE_SERVICE_REQUEST_FAILED);
+    }
 }
